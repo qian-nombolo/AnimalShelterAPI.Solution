d994b7ecde865397734d8aba0c9901eab8722cfb
nancyliqn91 <nancyliqn91@gmail.com>
2023-06-09 12:00:44 -0700

    add versions

--- AnimalApi/Program.cs
+++ AnimalApi/Program.cs
@@ -1,6 +1,11 @@
 using AnimalApi.Models;
 using Microsoft.EntityFrameworkCore;
 
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -19,7 +24,26 @@ builder.Services.AddDbContext<AnimalApiContext>(
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Add versionining
+builder.Services.AddApiVersioning(opt =>
+  {
+    opt.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1,0);
+    opt.AssumeDefaultVersionWhenUnspecified = true;
+    opt.ReportApiVersions = true;
+    opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(), new HeaderApiVersionReader("x-api-version"), new MediaTypeApiVersionReader("x-api-version"));
+
+  });
+
+builder.Services.AddVersionedApiExplorer(setup =>
+{
+  setup.GroupNameFormat = "'v'VVV";
+  setup.SubstituteApiVersionInUrl = true;
+});
+
+
 var app = builder.Build();
+
+var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
